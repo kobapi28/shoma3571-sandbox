0e5780a54339e96ac300cb3b13ceb45598523ee2
Shoma Kobayashi <sho1228.k@gmail.com>
2022-04-03 13:57:18 +0900

    add: typescript/nest-todo

---
+++ typescript/nest-todo/src/main.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // 環境変数の確認等
+  const configService = new ConfigService();
+  const sync = configService.get('DB_SYNC');
+  console.log(`TypeORM synchronize is [ ${sync} ]`);
+  const port = configService.get('API_PORT');
+
+  await app.listen(port);
+}
+bootstrap();
---
+++ typescript/nest-todo/src/tasks/tasks.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { TaskPropertyDto } from './dto/task-property.dto';
+import { TaskStatusPipe } from './pipe/task-status.pipe';
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(): Promise<Task[]> {
+    return this.tasksService.getTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() taskPropertyDto: TaskPropertyDto): Promise<Task> {
+    return this.tasksService.createTask(taskPropertyDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id')
+  updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusPipe) status: string,
+  ): Promise<Task> {
+    return this.tasksService.updateTask(id, status);
+  }
+}
